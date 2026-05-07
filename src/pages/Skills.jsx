88049178b8c9129806756a8a99a9512fba01c46a
Mani Amar Sehgal <sehgalmaniamar@gmail.com>
2026-05-07 15:46:31 +0530

    logic for skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,16 +1,44 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Skills(){
+
+    const [count,setcount]=useState(0)
+    const [Skills,setSkills]=useState([])
+    const [newSkill,setNewSkill]=useState("")
+
+
+    const addSkill=()=>{
+        if (newSkill.trim()==="") return;
+        const updatedSkill=[...Skills]
+        updatedSkill.push(newSkill)
+        setSkills(updatedSkill)
+        setNewSkill("")
+    }
+    
+
     return (
         <div className="flex flex-col p-6">
-            <div className="flex flex-col gap-3 p-4 mb-6">
+            <div className="flex flex-col gap-3 p-4 mb-4">
                 <h1 className="text-white font-bold text-4xl">Your Skills</h1>
                 <p>Add and manage your skills to get better recommendations.</p>
             </div>
-            <div className="flex p-4">
-                <input type="text" placeholder="Add a Skill and press Enter..." className="w-96 bg-white/5 p-3 rounded-lg"/>
+            <div className="flex px-4 py-2 gap-8 mb-10">
+                <input 
+                type="text" 
+                placeholder="Add a Skill and press Enter..." 
+                className="w-200 bg-white/5 p-3 rounded-lg"
+                value={newSkill}
+                onChange={(e)=>setNewSkill(e.target.value)}/>
+                <button className="bg-violet-500 text-white rounded-lg p-3" onClick={addSkill}>Add Skill</button>
             </div>
-            <div></div>
+            <div>
+                <h1>Your Skills ({count})</h1>
+                {Skills.map((skill)=>{
+                    return(<div key={skill.id}>{skill}</div>
+                    )
+                })}
+            </div>
 
         </div>
     )
